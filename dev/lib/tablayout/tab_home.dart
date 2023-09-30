ed102ef98f7031583194f9609b6ad3f3f3741153
Quy15 <hungquy10101996@gmail.com>
2023-09-30 20:23:57 +0700

    update auth and chat

--- dev/lib/tablayout/tab_home.dart
+++ dev/lib/tablayout/tab_home.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dev/layout/pt_detail.dart';
+import 'package:dev/layout/select_pt.dart';
 import 'package:dev/tablayout/get_pt.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +16,12 @@ class _HomeTab extends State<HomeTab> {
   String id = " ";
   String uname = " ";
   String prequently = " ";
+  String weight = " ";
+  String height = " ";
+  double bmi = 0;
+  double cn = 0;
+  double ccao = 0;
+  String BMI = " ";
   Future getUserByEmail(String? email) async {
     await FirebaseFirestore.instance
         .collection("users")
@@ -26,7 +32,13 @@ class _HomeTab extends State<HomeTab> {
         setState(() {
           id = '${doc['id']}';
           this.uname = '${doc['name']}';
+          this.weight = '${doc['weight(kg)']}';
+          this.height = '${doc['height(cm)']}';
+          this.cn = double.parse(this.weight);
+          this.ccao = double.parse(this.height);
           print(id);
+          print(weight);
+          print(height);
         });
       });
     });
@@ -43,6 +55,12 @@ class _HomeTab extends State<HomeTab> {
         print(prequently);
       });
     });
+  }
+
+  String bmiCal() {
+    bmi = cn / (ccao * ccao);
+    BMI = bmi.toStringAsFixed(2);
+    return BMI;
   }
 
   // Future getPurpose() async {
@@ -62,15 +80,15 @@ class _HomeTab extends State<HomeTab> {
 
   Future getPT() async {
     await FirebaseFirestore.instance
-          .collection("trainers")
-          .where("active", isEqualTo: true)
-          .where("teachdays", isEqualTo: prequently)
-          .get()
-          .then((QuerySnapshot snapshot){
-            snapshot.docs.forEach((DocumentSnapshot doc) { 
-              ptIDs.add(doc.reference.id);
-            });
-          });
+        .collection("trainers")
+        .where("active", isEqualTo: true)
+        .where("teachdays", isEqualTo: prequently)
+        .get()
+        .then((QuerySnapshot snapshot) {
+      snapshot.docs.forEach((DocumentSnapshot doc) {
+        ptIDs.add(doc.reference.id);
+      });
+    });
   }
 
   Future<List<QueryDocumentSnapshot>> getPTByPurpose() async {
@@ -94,32 +112,147 @@ class _HomeTab extends State<HomeTab> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.sizeOf(context);
-    return Scaffold(
-      body: FutureBuilder(
-        future: getPT(),
-        builder: (context, snapshot) {
-            return ListView.builder(
-              itemCount: ptIDs.length,
-              itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Container(
-                      height: 120,
-                      margin: EdgeInsets.all(8.0),
-                      padding: EdgeInsets.all(12.0),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      child: GetPT(ptID: ptIDs[index]),
+    return SafeArea(
+      child: Scaffold(
+          body: Padding(
+        padding: const EdgeInsets.only(left: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Xin chào " + uname,
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                  ),
+                  CircleAvatar(
+                    child: Icon(
+                      Icons.person,
+                      size: 40,
                     ),
-                    onTap: (){
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => PTDetail()));
-                    },
-                  );
-              },
-            );
-        },
-      ),
+                    radius: 30,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Chỉ số cơ thể',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 500,
+              height: 280,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromARGB(255, 183, 164, 238)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text(
+                              "Cân nặng: ",
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          ),
+                          Text(
+                            weight + " kg",
+                            style: TextStyle(fontSize: 25),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "Chiều cao: ",
+                            style: TextStyle(fontSize: 25),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 20),
+                            child: Text(
+                              height + " m",
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text("BMI: " + bmiCal(),
+                        style: TextStyle(fontSize: 25)),
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text("TDEE: ", style: TextStyle(fontSize: 25)),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Tiện ích dành cho bạn',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Column(
+              children: [
+                Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.12),
+                          blurRadius: 5.0,
+                          spreadRadius: 1.1,
+                        ),
+                      ]),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => ChoosePT()));
+                      },
+                      child: Icon(Icons.list_alt_outlined)
+                      ), 
+                ),
+                SizedBox(height: 5,),
+                Text('PT được đề xuất', style: TextStyle(fontSize: 20),),
+              ],
+            )
+          ],
+        ),
+      )),
     );
   }
 }
